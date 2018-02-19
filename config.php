a1489e054b5d5f6189453cf79d4207faee153915
peterchabs <chabot.peter@gmail.com>
2018-02-19 08:03:03 -0600

    Add files via upload

--- config.php
+++ config.php
@@ -9,13 +9,13 @@
 # EDIT ME PLEASE
 
 // mysql db name
-define('SYS_DB_NAME', '#SYS_DB_NAME#');
+define('SYS_DB_NAME', 'mwp');
 // mysql username
-define('SYS_DB_USER', '#SYS_DB_USER#');
+define('SYS_DB_USER', 'root');
 // mysql password
-define('SYS_DB_PSWD', '#SYS_DB_PSWD#');
+define('SYS_DB_PSWD', 'Pjc&drk1337');
 // mysql server name
-define('SYS_DB_HOST', '#SYS_DB_HOST#');
+define('SYS_DB_HOST', '127.0.0.1');
 // mysql server port
 define('SYS_DB_PORT', 3306);
 
--- index_gymhistory.php
+++ index_gymhistory.php
@@ -16,6 +16,15 @@ include_once('core/process/data.loader.php');
     enable_page_level_ads: true
   });
 </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-98689301-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-98689301-2');
+</script>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
